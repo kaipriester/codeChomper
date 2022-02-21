918377358a87dc185b7f38d17bcc639484bd2752
Pablo Estrada <pablof300@gmail.com>
2022-02-21 13:04:24 -0500

    added file upload

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const app = express()
 const port = 8080
 
 const { ESLint } = require("eslint");
+const fs = require('fs');
+const StreamZip = require('node-stream-zip');
 
 var corsOptions = {
   origin: '*',
@@ -26,19 +28,48 @@ app.post('/upload', async (req, res) => {
   const file = req.files.file;
   const filename = file.name;
 
-  console.log(file)
+  const fileLocation = `${'testFiles/'}${filename}`;
 
-  file.mv(`${'testFiles/'}${filename}`, async (err) => {
-    console.log(err)
+  if (!fs.existsSync('extracted')) {
+    fs.mkdirSync('extracted');
+  }
 
+  file.mv(fileLocation, async (err) => {
     const eslint = new ESLint();
-    const results = await eslint.lintFiles(["testFiles/**/*.js"])
-    const formatter = await eslint.loadFormatter("stylish");
-    // const resultText = formatter.format(results);
-  
+
+    // const eslint = new ESLint();
+    // const results = await eslint.lintFiles(["testFiles/**/*.js"])
+    // const formatter = await eslint.loadFormatter("stylish");
+
+    // await new Promise(resolve => setTimeout(resolve, 5000));
+    // const zip = new StreamZip({file: fileLocation, storeEntries: true});
+    // try {
+    //   console.log(`doing it for ${fileLocation}`)
+    //   const count = await zip.extract(null, './extracted');
+    // } catch (e) {
+    //   console.log("ERror")
+    //   console.log(e)
+    // }
+
+    var AdmZip = require("adm-zip");
+
+// reading archives
+    var zip = new AdmZip(fileLocation);
+    // var zipEntries = zip.getEntries(); // an array of ZipEntry records
+    zip.extractAllTo('./extracted', /*overwrite*/ true);
+
+    const results = await eslint.lintFiles(["./extracted/**/*.js"]);
+
+    results.forEach(result => {
+      console.log(result.messages)
+    })
+
+    // console.log(results);
+
     // 4. Output it.
-    console.log(results);
+    // const resultText = formatter.format(results);
     // console.log(JSON.stringify(resultText))
+    console.log("WOPRKED GREAT@@R@")
   });
 
   res.send("worked :)")
