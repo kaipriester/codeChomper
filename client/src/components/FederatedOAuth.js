ecb93aa8c44b2aae34725524a58914cc2beff407
claudia-rubio <68291719+claudia-rubio@users.noreply.github.com>
2022-11-02 01:03:44 -0400

    Google and Facebook OAuth UI and API call fixes

--- client/src/components/FederatedOAuth.js
+++ client/src/components/FederatedOAuth.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import FacebookLogin from "react-facebook-login";
+import jwt_decode from "jwt-decode";
 
 function FederatedOAuth() {
 
@@ -7,18 +8,37 @@ function FederatedOAuth() {
     console.log(response);
   };
 
+  const responseGoogle = (response) => {
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+  };
+
   const componentClicked = () => console.log("clicked");
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "390826259140-6n1kgjgmacpeme764caiu15ovtkfua1j.apps.googleusercontent.com",
+      callback: responseGoogle
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("googleDiv"),
+      { theme: "outline", size: "large"}
+    );
+  });
 
   return (<div>
       <FacebookLogin
-          appId={process.env.FACEBOOK_APP_ID}
-          autoLoad={true}
+          appId="1179351039598355"
+          reauthenticate={true}
           fields="name,email"
           onClick={componentClicked}
           callback={responseFacebook}
           icon="fa-facebook"
           size="small"
         />
+      <div id="googleDiv"></div>
     </div>
     );
 };
